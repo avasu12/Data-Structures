845591597989ba6b7f3343fc0782ae5e9d9076fd
Ajay Vasu <ajay_vasu@outlook.com>
2024-08-22 11:35:02 +0530

    Set queries & modifications

--- ds_sets.py
+++ ds_sets.py
@@ -1,1 +1,23 @@
-# test
+# Sets
+
+# Sets can be dynamic - grow, shrink, change
+# Sets are immutable, and contain no duplicates
+
+random_set = {0, 1, 1, 2, 3, 5, 8}
+
+print(random_set)
+
+''' 
+Categories of operations on sets: 
+1. Queries
+2. Modifications
+'''
+
+print(len(random_set))
+another_set = set((1, "five", False, 365, 0))
+print(another_set)
+print(len(another_set))
+another_set.pop()
+print(another_set)
+another_set.add(3.14)
+print(another_set)
