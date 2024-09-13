7985b1b829c69a1acc00778645c586a509bdbffd
Ajay Vasu <ajay_vasu@outlook.com>
2024-09-13 20:02:11 +0530

    Simple statements - assert & pass

---
+++ language_ref.py
@@ -0,0 +1,23 @@
+
+
+
+# An assert statement
+
+variable_one = "string"
+variable_two = 'string'
+
+
+assert variable_one == variable_two
+
+
+# The pass keyword
+
+def calculate_distance(point_a: int = 17, point_b: int = 9) -> int:
+    pass
+    pass
+    pass
+    pass
+
+pass
+pass
+
