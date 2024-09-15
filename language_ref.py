935c3b77cf5c9baceb60d15b1179b345dfaf8f2c
Ajay Vasu <ajay_vasu@outlook.com>
2024-09-15 23:36:54 +0530

    New statements - del & global

--- language_ref.py
+++ language_ref.py
@@ -21,3 +21,20 @@ def calculate_distance(point_a: int = 17, point_b: int = 9) -> int:
 pass
 pass
 
+# del statement
+
+x = 10
+del x
+my_list = ["avocado", "potato", "leek", "lettuce"]
+del my_list[1]
+print(my_list)
+
+# The global statement
+y = 15
+def write_message():
+    global y
+    y = 20
+    print(y)
+
+write_message()
+print(y)
