d1d228d91992b778ca83dc889bb2d6ba8a0ba251
Ajay Vasu <ajay_vasu@outlook.com>
2024-09-19 23:03:20 +0530

    Generator function test

--- language_ref.py
+++ language_ref.py
@@ -12,7 +12,7 @@ assert variable_one == variable_two
 integer_var = 17
 float_var = 18.0
 
-assert integer_var > float_var, "Assertion message"
+assert integer_var < float_var, "Assertion message"
 
 
 # The pass keyword
@@ -58,3 +58,14 @@ with open('algorithms.py', 'r') as file:
 
 type Coordinate = tuple[float, float]
 print(Coordinate)
+
+# The yield statement
+
+def count_stuff():
+    count = 5
+    while count > 0:
+        yield count
+        count-=1
+
+yield_response = count_stuff()
+print(yield_response)
